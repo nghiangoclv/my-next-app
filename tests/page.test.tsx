1b3d455d51473e2eedc094c7f201a335e2355179
nghia.nt <nghia.ngo.clv@gmail.com>
2025-01-08 21:28:48 +0700

    update test

--- tests/page.test.tsx
+++ tests/page.test.tsx
@@ -34,7 +34,7 @@ describe('Ocean Page', () => {
       possibleCreatures.includes(content)
     );
 
-    expect(creature).toBeUndefined();
+    expect(creature).toBeDefined();
   });
 
   // it('renders two wave divs with correct classes and animations', () => {

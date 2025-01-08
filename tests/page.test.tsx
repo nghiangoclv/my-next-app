b4f7ca4e398a6c1457fd53d24165c757b762062f
nghia.nt <nghia.ngo.clv@gmail.com>
2025-01-08 22:37:11 +0700

    update test

--- tests/page.test.tsx
+++ tests/page.test.tsx
@@ -1,6 +1,7 @@
-import OceanPage from '@/pages/ocean';
 import '@testing-library/jest-dom';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+
+import OceanPage from '@/pages/ocean';
 
 describe('Ocean Page', () => {
   it('renders the sky with the sun emoji', () => {
@@ -9,48 +10,27 @@ describe('Ocean Page', () => {
     const sun = screen.getByText('☀️');
     expect(sun).toBeInTheDocument();
   });
+  
+  it('renders the correct ocean creatures', () => {
+    render(<OceanPage />);
+
+    const creatures = screen.getAllByText(/🐟|🐠|🐳/);
+    expect(creatures).toHaveLength(3);
+  });
 
   it('renders the sand with the beach emoji', () => {
     render(<OceanPage />);
 
-    const beach = screen.getByText('🏖️');
-    expect(beach).toBeInTheDocument();
+    const sand = screen.getByText('🏖️');
+    expect(sand).toBeInTheDocument();
   });
 
-  it('handles sand click event', () => {
+  it('checks if clicking sand triggers the correct alert', () => {
     render(<OceanPage />);
 
     const sand = screen.getByText('🏖️');
     window.alert = jest.fn(); // Mock the alert function
-    fireEvent.click(sand);
+    sand.click();
     expect(window.alert).toHaveBeenCalledWith('You clicked the sand!');
-  });
-
-  it('renders one of the ocean creatures (🐟, 🐠, or 🐳)', () => {
-    render(<OceanPage />);
-
-    const possibleCreatures = ['🐟', '🐠', '🐳'];
-    const creature = screen.getByText((content, element) =>
-      possibleCreatures.includes(content)
-    );
-
-    expect(creature).toBeDefined();
-  });
-
-  // it('renders two wave divs with correct classes and animations', () => {
-  //   render(<OceanPage />);
-
-  //   const waves = screen.getAllByClassName(/wave-animation/);
-  //   expect(waves).toHaveLength(2);
-
-  //   expect(waves[0]).toHaveClass('wave-animation');
-  //   expect(waves[1]).toHaveClass('wave-animation-second');
-  // });
-
-  it('renders the ocean section with the correct background', () => {
-    render(<OceanPage />);
-
-    // const ocean = screen.getByRole('region', { name: 'Ocean' }); // Add role="region" and aria-label="Ocean" for accessibility in your OceanPage component
-    // expect(ocean).toHaveStyle('background-color: rgb(59 130 246)'); // Tailwind bg-blue-500
   });
 });
